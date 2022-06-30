63c7cb76f54c85007efb4bc41d9d1433608d4b67
phanindravedula <phanindravedula@gmail.com>
2022-06-30 10:00:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
 from flask_mysqldb import MySQL
+import logging
+
 app = Flask(__name__, template_folder='./templates/')
 
 
@@ -20,5 +22,6 @@ def index():
         cur.execute("INSERT INTO MyUsers (FirstName, LastName) VALUES (%s, %s)", (firstName, lastName))
         mysql.connection.commit()
         cur.close()
+        logging.info ('Success')
         return 'success'
     return render_template('index.html')
